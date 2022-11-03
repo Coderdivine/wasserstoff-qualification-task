ccd0fb0ace9854c34e00ac815822f89955964291
Chimdi <87563898+Coderdivine@users.noreply.github.com>
2022-11-03 12:11:34 +0100

    Code explained

--- DBConnection/index.js
+++ DBConnection/index.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-const db = mongoose.connection
+const db = mongoose.connection // instantiate a mongodb connection using mongoose(a mongodb library)
 db.on("error",(err)=>{console.log(err)})
 db.once("open",()=> console.log("Connected to database"))
