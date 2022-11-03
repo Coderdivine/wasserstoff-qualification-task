37435fe6b55c5c7a9109e04d85f8bbcb8c4e697b
Chimdi <87563898+Coderdivine@users.noreply.github.com>
2022-11-03 11:46:31 +0100

    Update some comments

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json()) //code accepts json requests
 app.use(express.urlencoded({ extended: true }));
 const URI = 'mongodb+srv://chimdi:chimdindu2@cluster0.5zspaed.mongodb.net/?retryWrites=true&w=majority' || process.env.AUTH_URI || 'mongodb://localhost:27017/users';
-//The URI is may personal URI
+//The URI is may personal URI. It's public 
 mongoose.connect(URI);
 const port = process.env.PORT || 1369;
 require("./DBConnection"); // this codes connect to mongodb database 
